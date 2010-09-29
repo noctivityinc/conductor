aeeaf35f27504d44839a06978fee44a6b00ebeb8
Noctivity <jlippiner@noctivity.com>
2010-09-29 11:14:56 -0400

    changed versioning to pull from gemspect

--- lib/conductor.rb
+++ lib/conductor.rb
@@ -11,10 +11,13 @@ class Conductor
   MINIMUM_LAUNCH_DAYS = 7
   DBG = false
 
-  @@VERSION = "0.1.0"
-  @@MAJOR_VERSION = "1.0"
-  cattr_reader :VERSION
-  cattr_reader :MAJOR_VERSION
+  module Version
+    version = Gem::Specification.load(File.expand_path("../conductor.gemspec", File.dirname(__FILE__))).version.to_s.split(".").map { |i| i.to_i }
+    MAJOR = version[0]
+    MINOR = version[1]
+    PATCH = version[2]
+    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
+  end
 
   cattr_writer :cache
 
