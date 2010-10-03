e4f9e3dd7b069943ab3f961cc694037b0904b1b4
Noctivity <jlippiner@noctivity.com>
2010-10-03 17:23:33 -0400

    configurable attribute_for_weighting and days_till_weighting.  now using weighted moving averge to compute weights

--- lib/conductor/weights.rb
+++ lib/conductor/weights.rb
@@ -6,7 +6,7 @@ class Conductor
       # group do not match all the alternatives previously computed for the group, new weights are
       # generated.  The cache is used to speed up this check
       def find_or_create(group_name, alternatives)
-        weights_for_group = Conductor.cache.read("Conductor::Experiment::#{group_name}::Alternatives")
+        weights_for_group = Conductor.cache.read("Conductor::Experiment::#{Conductor.attribute_for_weighting}::#{group_name}::Alternatives")
 
         alternatives_array = weights_for_group.map(&:alternative).sort if weights_for_group
         if alternatives_array.eql?(alternatives.sort)
@@ -18,8 +18,8 @@ class Conductor
 
           # get the new weights
           weights_for_group = Conductor::Experiment::Weight.find(:all, :conditions => "group_name = '#{group_name}'")
-          Conductor.cache.delete("Conductor::Experiment::#{group_name}::Alternatives")
-          Conductor.cache.write("Conductor::Experiment::#{group_name}::Alternatives", weights_for_group)
+          Conductor.cache.delete("Conductor::Experiment::#{Conductor.attribute_for_weighting}::#{group_name}::Alternatives")
+          Conductor.cache.write("Conductor::Experiment::#{Conductor.attribute_for_weighting}::#{group_name}::Alternatives", weights_for_group)
           return weights_for_group
         end
       end
@@ -33,7 +33,7 @@ class Conductor
 
         unless group_rows.empty?
           Conductor::Experiment::Weight.delete_all(:group_name => group_name) # => remove all old data for group
-          total = group_rows.sum_it(:conversion_value)
+          total = group_rows.sum_it(Conductor.attribute_for_weighting)
           data = total ? compute_weights_for_group(group_name, group_rows, total) : assign_equal_weights(group_rows)
           update_weights_in_db(group_name, data)
         end
@@ -46,46 +46,50 @@ class Conductor
         def compute_weights_for_group(group_name, group_rows, total)
           data = []
           recently_launched = []
-          max_weight = 0
+          
+          # calculate weights for each alternative
+          weighted_moving_avg, total, max_weight = compute_weighted_moving_stats(group_rows)
 
           group_rows.group_by(&:alternative).each do |alternative_name, alternatives|
-            first_found_date = alternatives.map(&:activity_date).sort.first
-            days_ago = Date.today - first_found_date
+            days_ago = compute_days_ago(alternatives)
 
-            if days_ago >= MINIMUM_LAUNCH_DAYS
-              data << compute_weight_for_alternative(alternative_name, alternatives, max_weight, total)
+            if days_ago >= Conductor.minimum_launch_days
+              data << {:name => alternative_name, :weight => (weighted_moving_avg[alternative_name] / total)}
             else
-              Conductor.log("adding #{alternative_name} to recently launched array")
               recently_launched << {:name => alternative_name, :days_ago => days_ago}
             end
           end
 
-          data += weight_recently_launched(max_weight, recently_launched)
+          weight_recently_launched(data, max_weight, recently_launched)
           return data
         end
 
-        def compute_weight_for_alternative(alternative_name, alternatives, max_weight, total)
-          Conductor.log("compute_weight_for_alternative for #{alternative_name}")
-
-          aggregates = {:name => alternative_name}
-
-          weight = alternatives.sum_it(:conversion_value) / total
-          max_weight = weight if weight > max_weight
-          aggregates.merge!({:weight => weight})
-
-          return aggregates
+        def compute_weighted_moving_stats(group_rows)
+          weighted_moving_avg = {}
+          group_rows.group_by(&:alternative).each do |alternative_name, alternatives|
+            weighted_moving_avg.merge!({"#{alternative_name}" => alternatives.sort_by(&:activity_date).weighted_mean_of_attribute(Conductor.attribute_for_weighting)}) 
+          end
+          
+          # calculate total and max_weight for weighted moving averages
+          total = weighted_moving_avg.values.sum
+          max_weight = weighted_moving_avg.values.max
+          
+          return weighted_moving_avg, total, max_weight
+        end
+        
+        def compute_days_ago(alternatives)
+          first_found_date = alternatives.map(&:activity_date).min
+          (Date.today - first_found_date.to_date).to_f
         end
 
-        def weight_recently_launched(max_weight, recently_launched)
+        def weight_recently_launched(data, max_weight, recently_launched)
           # loop through recently_launched to create weights for table
           # the handicap sets a newly launched item to the max weight * MAX_WEIGHTING_FACTOR and then
           # slowly lowers its power until the launch period is over
-          data = []
-          max_weight = 0 ? 1 : max_weight # => if a max weight could not be computed, set it to 1
-          Conductor.log("max weight: #{max_weight}")
+          max_weight = 1 if data.empty?
           recently_launched.each do |alternative|
-            handicap = (alternative[:days_ago].to_f / MINIMUM_LAUNCH_DAYS)
-            launch_window = (MINIMUM_LAUNCH_DAYS - alternative[:days_ago]) if MINIMUM_LAUNCH_DAYS > alternative[:days_ago]
+            handicap = (alternative[:days_ago].to_f / Conductor.minimum_launch_days)
+            launch_window = (Conductor.minimum_launch_days - alternative[:days_ago]) if Conductor.minimum_launch_days > alternative[:days_ago]
             Conductor.log("Handicap for #{alternative[:name]} is #{handicap} (#{alternative[:days_ago]} days ago)")
             data << {:name => alternative[:name], :weight => max_weight * MAX_WEIGHTING_FACTOR * (1 - handicap), :launch_window => launch_window}
           end
